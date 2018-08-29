a55b185993277eddb1434654b4d11da91c9d10cf
Chernysh Vladimir <chernysh.vladimir@apriorit.com>
2018-08-29 21:14:50 +0300

    prepared environment

---
+++ ClonesTests/main.cpp
@@ -0,0 +1,11 @@
+// ClonesTests.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "Clones.h"
+
+int main(int argc, char* argv[])
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
